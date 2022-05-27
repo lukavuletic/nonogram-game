5921463ff306a67edaae9485844ad170b802cb93
lvuletic <luka.vuletic1999@gmail.com>
2022-05-27 22:00:00 +0200

    implement identify pixel in an img, introduce jquery

--- src/modules/GameScreen/pages/GameScreen.jsx
+++ src/modules/GameScreen/pages/GameScreen.jsx
@@ -4,12 +4,18 @@ import { inject, observer } from "mobx-react";
 import { GameScreenStore } from "@stores";
 
 export const GameScreen = inject(() => ({ gameScreenStore: new GameScreenStore() }))(
-    observer(({ gameScreenStore: { text } }) => {
+    observer(({ gameScreenStore: { text, showNgMatrix } }) => {
         return (
             <div className="App">
                 <header className="App-header">
                     {text}
                 </header>
+                <div>
+                    <button type="button" onClick={() => showNgMatrix()}>
+                        show monogram
+                    </button>
+                    <canvas id="mycanvas" />
+                </div>
             </div>
         )
     })
--- src/stores/GameScreen/GameScreenStore.js
+++ src/stores/GameScreen/GameScreenStore.js
@@ -1,4 +1,5 @@
-import { makeObservable, observable } from "mobx";
+import { makeObservable, observable, action, computed } from "mobx";
+import $ from "jquery";
 
 export class GameScreenStore {
     textVal = "text";
@@ -6,10 +7,32 @@ export class GameScreenStore {
     constructor() {
         makeObservable(this, {
             textVal: observable,
+            showNgMatrix: action.bound,
+            text: computed,
         });
     }
 
     get text() {
         return this.textVal;
+    }
+
+    showNgMatrix() {
+        const canvas = document.getElementById('mycanvas');
+        const cx = canvas.getContext('2d');
+
+        const myImg = new Image();
+        myImg.src = require("../../resources/nonogram0.png");
+
+        $(myImg).on("load", () => {
+            cx.drawImage(myImg, 2, 1);
+            const pixel = cx.getImageData(0, 0, 10, 10);
+            const data = pixel.data;
+
+            if (data[0] === 0 && data[1] === 0 && data[2] === 0) {
+                this.textVal = "black pixel";
+            } else if (data[0] === 255 && data[1] === 255 && data[2] === 255) {
+                this.textVal = "white pixel";
+            }
+        });
     }
 }
